66f4401a130858c61f3622fe20f78848da4ebc1e
Soulbolt <soulbolt@gmail.com>
2024-04-29 21:30:28 -0700

    Add menu/logo container, add logo img src, Add #hero and @media background configuration to input.css file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./*.html'],
+  content: ["./*.html"],
   theme: {
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
     extend: {
-      spacing: {
-        '128': '32rem',
-      }
+      fontFamily: {
+        sans: ["Josefin Sans", "sans-serif"],
+        alata: ["Alata", "sans-serif"],
+      },
     },
   },
   plugins: [],
-}
-
+};
